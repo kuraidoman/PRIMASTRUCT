cc12421f70ef9212d8bbc9feccf156d6537a278a
ruruxhico <daniellejanm.15@gmail.com>
2025-08-18 12:57:23 +0800

    index code

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prima-struct Engineering Consultancy</title>
+    <link rel="stylesheet" href="styles.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" 
+    integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" 
+    crossorigin="anonymous">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"/>
+
+</head>
+<body>
+    <div id="start"></div>
+    <script>
+        function note() 
+        {
+          alert("The account is still under construction. Come by next time!");
+        }
+        </script>
+    <!-- header -->
+    <header>
+        <nav class="header">
+            <a class="navbar-brand" href="#">
+                <img class="logo" src="./pictures finals/strawberry-svgrepo-com.svg"/> 
+                <span>&ensp;Prima-struct Engineering Consultancy</span> 
+            </a>
+        </nav>
+        <nav class="navigation">
+            <a href="home.html">Home</a>
+            <a href="home.html#works">About</a>
+            <a href="home.html#works">Services</a>
+            <a href="index.html">Logout</a>
+        </nav>
+    </header> 
+    <!-- End header -->
+</body>
+
+</html>
