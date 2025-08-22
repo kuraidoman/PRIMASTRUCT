9ec4b47ba678695cbee37bdbc4dc365ecbe04f4c
ruruxhico <daniellejanm.15@gmail.com>
2025-08-22 18:42:26 +0800

    with header and footer

---
+++ header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prima-struct Engineering Consultancy</title>
+    <link rel="stylesheet" href="styles.css">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" 
+    integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" 
+    crossorigin="anonymous">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet"/>
+</head>
+<body>
+    <header>
+        <nav class="header">
+            <a class="navbar-brand" href="#">
+                <img class="logo" src="assets/logo.png"/> 
+                <span>&ensp;Prima-struct Engineering Consultancy</span> 
+            </a>
+        </nav>
+        <nav class="navigation">
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="services.php">Services</a>
+            <a href="contact_us.php">Contact Us</a>
+        </nav>
+    </header> 
+</body>
+</html>
